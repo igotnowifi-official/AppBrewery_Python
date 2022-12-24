89d058da2a1025421264b29b687740df029824c3
igotnowifi <liew.natasya@gmail.com>
2022-12-24 10:19:56 -0800

    upload 5.2 - partial due to xmas holiday

--- Professional Project/Day 91 - Image Processing and Data Science/main.py
+++ Professional Project/Day 91 - Image Processing and Data Science/main.py
@@ -1,85 +1,86 @@
+from tkinter import *
+from tkinter import filedialog
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.image as mpimg
-
 from PIL import Image
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from scipy.spatial import KDTree
+from webcolors import (
+    CSS3_HEX_TO_NAMES,
+    hex_to_rgb,
+)
 
-#!pip install easydev                 #version 0.12.0
-#!pip install colormap                #version 1.0.4
-#!pip install opencv-python           #version 4.5.5.64
-#!pip install colorgram.py            #version 1.2.0
-#!pip install extcolors               #version 1.0.0
-import cv2
-import extcolors
-
-from colormap import rgb2hex
+NUMBER_OF_COLORS = 15
+TITLE_FONT = "Papyrus"
+TITLE_COLOR = "blue"
+WORD_FONT = "Times New Roman"
+WORD_COLOR = "black"
+BACKGROUND_COLOR = "white"
+BTN_BACKGROUND_COLOR = "blue"
 
 
-def exact_color(input_image, resize, tolerance, zoom):
-    #background
-    bg = 'bg.png'
-    fig, ax = plt.subplots(figsize=(192,108),dpi=10)
-    fig.set_facecolor('white')
-    plt.savefig(bg)
-    plt.close(fig)
-    
-    #resize
-    output_width = resize
-    img = Image.open(input_image)
-    if img.size[0] >= resize:
-        wpercent = (output_width/float(img.size[0]))
-        hsize = int((float(img.size[1])*float(wpercent)))
-        img = img.resize((output_width,hsize), Image.ANTIALIAS)
-        resize_name = 'resize_'+ input_image
-        img.save(resize_name)
-    else:
-        resize_name = input_image
-    
-    #crate dataframe
-    img_url = resize_name
-    colors_x = extcolors.extract_from_path(img_url, tolerance = tolerance, limit = 13)
-    df_color = color_to_df(colors_x)
-    
-    #annotate text
-    list_color = list(df_color['c_code'])
-    list_precent = [int(i) for i in list(df_color['occurence'])]
-    text_c = [c + ' ' + str(round(p*100/sum(list_precent),1)) +'%' for c, p in zip(list_color, list_precent)]
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(160,120), dpi = 10)
-    
-    #donut plot
-    wedges, text = ax1.pie(list_precent,
-                           labels= text_c,
-                           labeldistance= 1.05,
-                           colors = list_color,
-                           textprops={'fontsize': 150, 'color':'black'})
-    plt.setp(wedges, width=0.3)
+def get_img_colors():
+    img_file = filedialog.askopenfilename(
+        initialdir="/",
+        title="Select IMG",
+        filetypes=(('Image Files', "*.jpg;*.jpeg;*.png"), ("All Files", "*.*"))
+    )
+    img = Image.open(img_file, 'r').convert('RGB')
+    color_codes = palette(img)
+    color_names = []
+    for code in color_codes:
+        if convert_rgb_to_names(code) not in color_names and len(color_names) < NUMBER_OF_COLORS:
+            color_names.append(convert_rgb_to_names(code))
+            colors_text.insert(END, convert_rgb_to_names(code) + '\n')
 
-    #add image in the center of donut plot
-    img = mpimg.imread(resize_name)
-    imagebox = OffsetImage(img, zoom=zoom)
-    ab = AnnotationBbox(imagebox, (0, 0))
-    ax1.add_artist(ab)
-    
-    #color palette
-    x_posi, y_posi, y_posi2 = 160, -170, -170
-    for c in list_color:
-        if list_color.index(c) <= 5:
-            y_posi += 180
-            rect = patches.Rectangle((x_posi, y_posi), 360, 160, facecolor = c)
-            ax2.add_patch(rect)
-            ax2.text(x = x_posi+400, y = y_posi+100, s = c, fontdict={'fontsize': 190})
-        else:
-            y_posi2 += 180
-            rect = patches.Rectangle((x_posi + 1000, y_posi2), 360, 160, facecolor = c)
-            ax2.add_artist(rect)
-            ax2.text(x = x_posi+1400, y = y_posi2+100, s = c, fontdict={'fontsize': 190})
 
-    fig.set_facecolor('white')
-    ax2.axis('off')
-    bg = plt.imread('bg.png')
-    plt.imshow(bg)       
-    plt.tight_layout()
-    return plt.show()
+def palette(img):
+    arr = np.asarray(img)
+    palette, index = np.unique(asvoid(arr).ravel(), return_inverse=True)
+    palette = palette.view(arr.dtype).reshape(-1, arr.shape[-1])
+    count = np.bincount(index)
+    order = np.argsort(count)
+    return palette[order[::-1]]
+
+
+def asvoid(arr):
+    arr = np.ascontiguousarray(arr)
+    return arr.view(np.dtype((np.void, arr.dtype.itemsize * arr.shape[-1])))
+
+
+def convert_rgb_to_names(rgb_tuple):
+    css3_db = CSS3_HEX_TO_NAMES
+    names = []
+    rgb_values = []
+    for color_hex, color_name in css3_db.items():
+        names.append(color_name)
+        rgb_values.append(hex_to_rgb(color_hex))
+
+    kdt_db = KDTree(rgb_values)
+    distance, index = kdt_db.query(rgb_tuple)
+    return names[index].title()
+
+
+def copy_to_clipboard():
+    root.clipboard_append(colors_text.get("1.0", END))
+
+
+def clear_text():
+    colors_text.delete("1.0", END)
+
+
+root = Tk()
+root.title('Image to Color List')
+root.config(padx=25, pady=25, bg=BACKGROUND_COLOR)
+title_label = Label(text="Image to Color List", font=(TITLE_FONT, 32, "bold"), fg=TITLE_COLOR, bg=BACKGROUND_COLOR)
+open_btn = Button(text="Select Image", font=(WORD_FONT, 20), width=25, fg=WORD_COLOR, bg=BTN_BACKGROUND_COLOR,
+                  command=get_img_colors)
+copy_btn = Button(text="Copy Text", font=(WORD_FONT, 20), width=25, fg=WORD_COLOR, bg=BTN_BACKGROUND_COLOR,
+                  command=copy_to_clipboard)
+clear_btn = Button(text="Clear Text", font=(WORD_FONT, 20), width=25, fg=WORD_COLOR, bg=BTN_BACKGROUND_COLOR,
+                   command=clear_text)
+colors_text = Text(root, width=35, height=16, font=(TITLE_FONT, 14))
+title_label.grid(column=0, row=0, pady=20)
+colors_text.grid(column=0, row=1)
+open_btn.grid(column=0, row=2, pady=10)
+copy_btn.grid(column=0, row=3)
+clear_btn.grid(column=0, row=4, pady=10)
+root.mainloop()
